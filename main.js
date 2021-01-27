d84721089ba19c2b37ec77d055a9c5581b68a350
unknown <mojnushah2018@gmai.com>
2021-01-27 14:59:41 +0600

    hello world

---
+++ main.js
@@ -0,0 +1,16 @@
+//login button event handler
+const loginBtn=document.getElementById("login");
+loginBtn.addEventListener("click",function(){
+   const loginArea=document.getElementById("login-area");
+   loginArea.style.display="none";
+    const transactionArea=document.getElementById("transaction-area");
+    transactionArea.style.display="block";
+    
+
+})
+//DepositBtn
+const depositBtn=document.getElementById("addDeposit");
+depositBtn.addEventListener("click",function(){
+    const depositAmount=document.getElementById("depositAmount").value;
+    const depositNumber=parseFloat(depositAmount);
+})
